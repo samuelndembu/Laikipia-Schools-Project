0b69b870affd1ca7a388f80b25a53849e680fbf6
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-02-25 09:36:51 +0300

    categories added

--- application/modules/laikipiaschools/views/partners/all_partners.php
+++ application/modules/laikipiaschools/views/partners/all_partners.php
@@ -5,10 +5,6 @@ if (!empty($validation_errors)) {
     echo $validation_errors;
 }
 ?>
-
-
-
-
 <div class="card shadow mb-4">
     <div class="card-header py-3">
 
@@ -57,11 +53,11 @@ if (!empty($validation_errors)) {
                                 placeholder="Enter email" name="partner_email">
                         </div>
                         <div class="form-group">
-                            <textarea rows="4" cols="50" for="partner_description">Partner Description</textarea>
-                            <input type="text" class="form-control" id="partner_description"
-                                aria-describedby="emailHelp" placeholder="Enter short description"
-                                name="partner_description">
+                            <label for="partner_description">Partner Description</label>
+                            <input type="text-area" class="form-control" id="partner_description" aria-describedby="emailHelp"
+                                placeholder="Enter sho description" name="partner_description">
                         </div>
+                       
                         <div class="form-group">
                             <label for="partner_logo">Upload Partner Logo</label>
                             <input type="file" class="form-control-file" id="partner_logo" name="partner_logo">
@@ -170,17 +166,17 @@ aria-hidden="true" >
 
         
         <?php if($row->partner_status == 1){
-        echo anchor("administration/edit/". $row->partner_id,"Edit","class='btn btn-warning btn-sm p-left-10'","style='padding-left:10px;'");
-        
-         
-          echo anchor("administration/deactivate-partner/". $row->partner_id . "/" .$row->partner_status ,"Deactivate","class='btn btn-primary btn-sm'");
+          
+          //echo anchor("administration/deactivate-partner/". $row->partner_id . "/" .$row->partner_status ,"Deactivate","class='btn btn-primary btn-sm'");
           echo anchor("administration/deactivate-partner/". $row->partner_id . "/" .$row->partner_status ,"DeActivate", array("class" => "btn btn-info btn-sm p-left-10", "onclick" => "return confirm('Are you sure you want to deactivate?')"));
         }
         else{
           echo anchor("administration/deactivate-partner/". $row->partner_id . "/" .$row->partner_status ,"Activate", array("class" => "btn btn-info btn-sm", "onclick" => "return confirm('Are you sure you want to activate?')"));
         }
-         echo anchor("administration/delete-partner/". $row->partner_id,"Delete","class='btn btn-danger btn-sm'");
-              echo anchor("administration/delete-partner/". $row->partner_id ,"Delete", array("class" => "btn btn-danger btn-sm", "onclick" => "return confirm('Are you sure you want to Delete?')"));?>
+         //echo anchor("administration/delete-partner/". $row->partner_id,"Delete","class='btn btn-danger btn-sm'");
+         echo anchor("administration/delete-partner/". $row->partner_id ,"Delete", array("class" => "btn btn-danger btn-sm", "onclick" => "return confirm('Are you sure you want to Delete?')"));
+         echo anchor("administration/edit/". $row->partner_id,"Edit","class='btn btn-warning btn-sm p-left-10'","style='padding-left:10px;'");
+         ?>
           
         </td>
       </tr>
@@ -199,119 +195,3 @@ aria-hidden="true" >
 
 
 
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>PartnerType</th>
-                            <th>PartnerName</th>
-                            <th>PartnerEmail</th>
-                            <th>PartnerLogo</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                        <!-- <tr><?php echo $links; ?></tr> -->
-                    </tfoot>
-                    <tbody>
-                        <?php
-if ($query->num_rows() > 0) {
-    $count = 0;
-    foreach ($query->result() as $row) {
-        $count++;
-        ?>
-                        <tr>
-                            <td>
-                                <?php echo $count ?>
-                            </td>
-                            <td>
-                                <?php echo $row->partner_type_name; ?>
-                            </td>
-                            <td>
-                                <?php echo $row->partner_name; ?>
-                            </td>
-                            <td>
-                                <?php echo $row->partner_email; ?>
-                            </td>
-                            <td>
-                                <img src="<?php echo base_url() . 'assets/uploads/' . $row->partner_thumb; ?>">
-                            </td>
-                            <td>
-
-                                <?php if ($row->partner_status == 1) {?>
-                                <a href="" class="btn btn-dark btn-sm" data-toggle="modal"
-                                    data-target="#modalLoginAvatar">View</a>
-                                <?php }?>
-
-                                <!--Modal: Login with Avatar Form-->
-                                <div class="modal fade" id="modalLoginAvatar" tabindex="-1" role="dialog"
-                                    aria-labelledby="myModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog cascading-modal modal-avatar modal-sm" role="document">
-                                        <!--Content-->
-                                        <div class="modal-content" style="margin-left:0px;">
-
-                                            <!--Header-->
-                                            <div class="modal-header">
-                                                <img src="<?php echo base_url() . 'assets/uploads/' . $row->partner_thumb; ?>"
-                                                    alt="avatar" class="rounded-circle img-responsive"
-                                                    style="margin-left:60px;">
-                                            </div>
-                                            <!--Body-->
-                                            <div class="modal-body text-center mb-1">
-
-                                                <h5 class="mt-1 mb-2">Retrieved <?php echo $row->partner_name; ?></h5>
-
-                                                <div class="md-form ml-0 mr-0"
-                                                    style="padding:30px;font-size:20px;list-style-type:none;margin-left:10px;">
-                                                    <li>Partner Type: <?php echo $row->partner_type_id; ?></li>
-                                                </div>
-                                                <div class="md-form ml-0 mr-0"
-                                                    style="padding:30px;font-size:20px;list-style-type:none;margin-left:10px;font-weight:bold;">
-                                                    <li>Partner Name:<?php echo $row->partner_name; ?></li>
-                                                </div>
-                                                <div class="md-form ml-0 mr-0"
-                                                    style="padding:30px;font-size:20px;list-style-type:none;margin-left:10px;">
-                                                    <li>Partner Email:<?php echo $row->partner_email; ?></li>
-                                                </div>
-                                                <div class="text-center mt-4">
-                                                    <button class="btn btn-cyan mt-1">Back <i
-                                                            class="fas fa-sign-in ml-1"></i></button>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                        <!--/.Content-->
-                                    </div>
-                                </div>
-                                <!--Modal: Login with Avatar Form-->
-
-
-
-                                <?php if ($row->partner_status == 1) {
-            echo anchor("administration/edit/" . $row->partner_id, "Edit", "class='btn btn-warning btn-sm'");
-
-            // echo anchor("administration/deactivate-partner/". $row->partner_id . "/" .$row->partner_status ,"Deactivate","class='btn btn-primary btn-sm'");
-            echo anchor("administration/deactivate-partner/" . $row->partner_id . "/" . $row->partner_status, "DeActivate", array("class" => "btn btn-info btn-sm", "onclick" => "return confirm('Are you sure you want to deactivate?')"));
-        } else {
-            echo anchor("administration/deactivate-partner/" . $row->partner_id . "/" . $row->partner_status, "Activate", array("class" => "btn btn-info btn-sm", "onclick" => "return confirm('Are you sure you want to activate?')"));
-        }
-        //echo anchor("administration/delete-partner/". $row->partner_id,"Delete","class='btn btn-danger btn-sm'");
-        echo anchor("administration/delete-partner/" . $row->partner_id, "Delete", array("class" => "btn btn-danger btn-sm", "onclick" => "return confirm('Are you sure you want to Delete?')"));?>
-
-                            </td>
-                        </tr>
-                        <?php
-}
-}
-?>
-                    </tbody>
-                </table>
-            </div>
-
-            <p>
-                <?php echo $links; ?>
-            </p>
-        </div>
-    
