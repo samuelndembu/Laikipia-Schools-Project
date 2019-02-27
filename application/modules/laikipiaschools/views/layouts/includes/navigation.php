a73b7e2051dde293c313b1c86ce0ad8692ed116d
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-02-27 11:16:42 +0300

    isset route

--- application/modules/laikipiaschools/views/layouts/includes/navigation.php
+++ application/modules/laikipiaschools/views/layouts/includes/navigation.php
@@ -28,16 +28,21 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         </ul>
-        <?php echo form_open(base_url() . 'administration/search-'.$route, array("class" => "form-inline my-2 my-lg-0")) ?>
-        <select class="custom-select2 form-control mr-sm-2" name="search_param">
-            <option>Choose..</option>
-            <?php foreach ($search_options as $search_option) {?>
-            <option value="<?php echo $search_option['id']; ?>"><?php echo $search_option['name']; ?></option>
-            <?php }?>
+        
+        <?php 
+        if(isset($route)){
+            echo form_open(base_url() . 'administration/search-'.$route, array("class" => "form-inline my-2 my-lg-0")) ?>
+                <select class="custom-select2 form-control mr-sm-2" name="search_param">
+                    <option>Choose..</option>
+                    <?php foreach ($search_options as $search_option) {?>
+                    <option value="<?php echo $search_option['id']; ?>"><?php echo $search_option['name']; ?></option>
+                    <?php }?>
 
-        </select>
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-        <?php echo form_close(); ?>
+                </select>
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            <?php echo form_close(); 
+            }?>
+       
     </div>
     <div class="dropdown mr-20 btn btn-primary my-2 my-sm-0 m-5">
         <a class="dropdown-toggle" href="<?php echo base_url(); ?>administration/logout" role="button"
